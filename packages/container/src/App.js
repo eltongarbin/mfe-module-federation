df78eb78b58bf29e724278105c574d3cedafe753
Elton Garbin <eltongarbin@gmail.com>
2022-12-02 14:20:02 -0300

    feat(container): add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hi there!!</h1>
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </div>
+    </BrowserRouter>
   );
 }
